c4fc5a801f7628f764359f09a5d91c1efcd108c9
Lazy Coder <adeolafadare8@gmail.com>
2022-10-28 16:37:46 +0100

    added fonts variables for stying and footer init

---
+++ react-app/src/components/molecules/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <footer id='site-footer'>
+                
+            </footer>
+        );
+    }
+}
+ 
+export default Footer;
