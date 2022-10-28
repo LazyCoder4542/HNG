8cb26682e19f06293289c38c38f1ab22ec7de15a
Lazy Coder <adeolafadare8@gmail.com>
2022-10-28 17:21:13 +0100

    created routes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import { Routes, Route, Link } from "react-router-dom";
+// importing pages
+import Home from './components/pages/Home';
 // importing components
 import Footer from './components/molecules/Footer';
-
 import './App.css';
 function App() {
   return (
     <>
     <div className='container'>
-      <section id="profile">
-        
-      </section>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<></>} />
+      </Routes>
     </div>
     <Footer />
     </>
---
+++ react-app/src/components/pages/Home.jsx
@@ -0,0 +1,20 @@
+export default function Home() {
+    return (
+        <section id="profile">
+            <div className="profile-pic">
+                <img src="" alt="profile-img" id="profile__img" className="pic" />
+            </div>
+            <div className="name">
+                <p className="fullname">Annette Black</p>
+                <p id="twitter">@MathFreak7</p>
+                <p id="slack" hidden></p>
+            </div>
+            <div className="links">
+            
+            </div>
+            <div className="socials">
+                <span></span>
+            </div>
+        </section>
+    )
+}
