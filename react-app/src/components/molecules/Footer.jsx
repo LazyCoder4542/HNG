03ae532f43e00d2c6f33e51344b8e6a287db5691
Lazy Coder <adeolafadare8@gmail.com>
2022-10-28 18:14:50 +0100

    completed footer jsx

--- react-app/src/components/molecules/Footer.jsx
+++ react-app/src/components/molecules/Footer.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-
+import {ReactComponent as ZuriLogo} from "./../../assets/icons/Zuri.svg"
+import {ReactComponent as I4GLogo} from "./../../assets/icons/I4G.svg"
 class Footer extends Component {
     state = {  } 
     render() { 
         return (
             <footer id='site-footer'>
-                
+                <span>
+                    <ZuriLogo />
+                </span>
+                <span>
+                    HNG Internship 9 Frontend Task
+                </span>
+                <span>
+                    <I4GLogo />
+                </span>
             </footer>
         );
     }
